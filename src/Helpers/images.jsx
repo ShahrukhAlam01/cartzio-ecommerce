7a9bdaa5c0df8abf9a7f2f94667f3c648e824de9
ShahrukhAlam01 <shahrukhalam286@gmail.com>
2025-07-15 23:47:51 +0500

    push

--- src/Helpers/images.jsx
+++ src/Helpers/images.jsx
@@ -6,6 +6,7 @@ export const shop = "./src/assets/images/shop.jpg";
 // Home One Banner Images Start //
 export const bg = "./src/assets/images/home1bg.png";
 // Home One Banner Images End //
+// Collection Json Start //
 import collection1 from '../assets/images/mens-ware.jpg';
 import collection2 from '../assets/images/ladies-ware.jpg'
 import collection3 from '../assets/images/kids-ware.jpg'
@@ -33,6 +34,189 @@ export const collections = [
         name:"Sunglasses"
     },
 ]
+// Collection Json End //
+// New Product Json Start //
+import product1 from '../assets/images/black-print-t-shirt.jpg'
+import product2 from '../assets/images/fashion-shoes-sneaker.jpg'
+import product3 from '../assets/images/men-white-espadrilles-slip-shoes.jpg'
+import product4 from '../assets/images/luxurious-bag.jpg'
+import product5 from '../assets/images/ladies-1.jpg'
+import product6 from '../assets/images/ladies-2.jpg'
+import product7 from '../assets/images/mens-jecket.jpg'
+import product8 from '../assets/images/sweater-t-shirt.jpg'
+import product9 from '../assets/images/trendy-t-shirt2.jpg'
+import product10 from '../assets/images/men-sun-glasses.jfif'
+import product11 from '../assets/images/women-s-white-low-heel-shoes.jpg'
+import product12 from '../assets/images/casual-men-shorts.jpg'
+import product13 from '../assets/images/luxurious-bag.jpg'
+import product14 from '../assets/images/smart-watch.jpg'
+import product15 from '../assets/images/men-white-espadrilles-slip-shoes.jpg'
+import product16 from '../assets/images/ladies-skirt-pair.jpg'
+export const newProduct =[
+    {
+        id:1,
+        rating:'one',
+        image:product1,
+        tag:'-40% Off',
+        name:'Black Print T-Shirt',
+        amount:'$21.00',
+        desRate:'$16.00',
+        offer:true,
+        desc:'Viverra a consectetur Go sporty this vintage navy and white striped.'
+    },
+    {
+        id:2,
+        rating: 'two',
+        image:product2,
+        tag:'New',
+        name:'Fashion Shoes Sneaker',
+        amount:'$21.00',
+        desRate:'$16.00',
+        desc:'Viverra a consectetur Go sporty this vintage navy and white striped.'
+    },
+    {
+        id:3,
+        rating: 'five',
+        image:product3,
+        tag:'',
+        name:'Mens White Slip Shoes',
+        amount:'$21.00',
+        desRate:'$16.00',
+        desc:'Viverra a consectetur Go sporty this vintage navy and white striped.'
+    },
+    {
+        id:4,
+        rating: 'three',
+        image:product4,
+        tag:'',
+        name:'Ladies Luxurious Bag',
+        amount:'$21.00',
+        desRate:'$16.00',
+        desc:'Viverra a consectetur Go sporty this vintage navy and white striped.'
+    },
+    {
+        id:5,
+        rating: 'five',
+        image:product5,
+        tag:'',
+        name:'Ladies Green Top',
+        amount:'$21.00',
+        desRate:'$16.00',
+        desc:'Viverra a consectetur Go sporty this vintage navy and white striped.'
+    },
+    {
+        id:6,
+        rating: 'four',
+        image:product6,
+        tag:'',
+        name:'Ladies Onepiece Wear',
+        amount:'$21.00',
+        desRate:'$16.00',
+        desc:'Viverra a consectetur Go sporty this vintage navy and white striped.'
+    },
+    {
+        id:7,
+        rating: 'five',
+        image:product7,
+        tag:'Featured',
+        name:'Mens Brown Jecket',
+        amount:'$21.00',
+        desRate:'$16.00',
+        desc:'Viverra a consectetur Go sporty this vintage navy and white striped.'
+    },
+    {
+        id:8,
+        rating: 'five',
+        image:product8,
+        tag:'Latest',
+        name:'Sweater T-Shirt',
+        amount:'$21.00',
+        desRate:'$16.00',
+        desc:'Viverra a consectetur Go sporty this vintage navy and white striped.'
+    },
+    {
+        id:9,
+        rating: 'one',
+        image:product9,
+        tag:'',
+        name:'Trendy T-shirt',
+        amount:'$21.00',
+        desRate:'$16.00',
+        desc:'Viverra a consectetur Go sporty this vintage navy and white striped.'
+    },
+    {
+        id:10,
+        rating: 'five',
+        image:product10,
+        tag:'New',
+        name:'Men Sun Glasses',
+        amount:'$21.00',
+        desRate:'$16.00',
+        desc:'Viverra a consectetur Go sporty this vintage navy and white striped.'
+    },
+    {
+        id:11,
+        rating: 'five',
+        image:product11,
+        tag:'-25% Off',
+        name:'Women White Low Heel Shoes',
+        amount:'$21.00',
+        desRate:'$16.00',
+        offer:true,
+        desc:'Viverra a consectetur Go sporty this vintage navy and white striped.'
+    },
+    {
+        id:12,
+        rating: 'five',
+        image:product12,
+        tag:'Featured',
+        name:'Black Print T-Shirt',
+        amount:'$21.00',
+        desRate:'$16.00',
+        desc:'Viverra a consectetur Go sporty this vintage navy and white striped.'
+    },
+    {
+        id:13,
+        rating: 'five',
+        image:product13,
+        tag:'New',
+        name:'Orange Luxurious Bag',
+        rate:'$21.00',
+        desRate:'$16.00',
+        desc:'Viverra a consectetur Go sporty this vintage navy and white striped.'
+    },
+    {
+        id:14,
+        rating: 'five',
+        image:product14,
+        tag:'Latest',
+        name:'Smart Watch',
+        rate:'$21.00',
+        desRate:'$16.00',
+        desc:'Viverra a consectetur Go sporty this vintage navy and white striped.'
+    },
+    {
+        id:15,
+        rating: 'five',
+        image:product15,
+        tag:'',
+        name:'White Sandals',
+        rate:'$21.00',
+        desRate:'$16.00',
+        desc:'Viverra a consectetur Go sporty this vintage navy and white striped.'
+    },
+    {
+        id:16,
+        rating: 'five',
+        image:product16,
+        tag:'',
+        name:'Ladies Skirt',
+        rate:'$21.00',
+        desRate:'$16.00',
+        desc:'Viverra a consectetur Go sporty this vintage navy and white striped.'
+    },
+]
+// New Product Json End //
 
 // Footer Start //
 import { FiShoppingCart, FiDribbble, FiLinkedin, FiFacebook, FiInstagram, FiTwitter, FiMail, FiGithub, FiYoutube, FiGitlab } from "react-icons/fi";
